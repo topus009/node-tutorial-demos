22dfd34dbb55970451428b52c6d1ce16f77e8f80
topus009 <sj_89@mail.ru>
2020-05-06 22:13:32 +0300

    eslint config fixes

--- 01-basic/first-app/main.js
+++ 01-basic/first-app/main.js
@@ -1,13 +1,12 @@
-'use strict';
 
-const utils = require(`./utils`);
+const utils = require('./utils');
 
-const alphabet = [`А`, `B`, `C`, `D`, `E`, `F`, `G`];
+const alphabet = ['А', 'B', 'C', 'D', 'E', 'F', 'G'];
 
 // Тасование массива
-console.log(`Тасование массива:`);
+console.log('Тасование массива:');
 console.log(utils.shuffle(alphabet));
 
 // Получение случайного числа
-console.log(`Получение случайного число от 1 до 10`);
+console.log('Получение случайного число от 1 до 10');
 console.log(utils.getRandomInt(1, 10));
